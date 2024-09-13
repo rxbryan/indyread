ba2f1bc88b1dea2f8f9b2ca5a8ef127777f7627c
rxbryan <rxbryn@gmail.com>
2024-09-13 13:40:51 +0100

    feat: suppor candy production indy network for dev test
    - add local candy_prod txs
    - support importing multiple indexes
    
    Signed-off-by: rxbryan <rxbryn@gmail.com>

--- start_test/dump_esindex.js
+++ start_test/dump_esindex.js
@@ -1,7 +1,7 @@
 const { exportIndex } = require('indyscan-storage')
 const { Client } = require('@elastic/elasticsearch')
 
-function run(url = "http://localhost:9200/", esindex = "txs-bcovdev", path=process.cwd()) {
+function run(url = "http://localhost:9200/", esindex = "txs-candyprod", path=process.cwd()) {
   const esClient = new Client({ node: url })
 
   const {dumpTxs} = exportIndex (esClient, esindex, path)
--- start_test/import_esindex.js
+++ start_test/import_esindex.js
@@ -2,17 +2,19 @@ const { importIndex } = require('indyscan-storage')
 const { Client } = require('@elastic/elasticsearch')
 const path = require('path')
 
-async function run(url = "http://localhost:9200/", esindex = "txs-bcovdev", path_str=process.cwd()) {
+async function run(url = "http://localhost:9200/", esindexes = ["txs-bcovdev", "txs-candyprod"], path_str=process.cwd()) {
   const esClient = new Client({ node: url })
 
-  const { importTxs } = await importIndex (esClient, esindex, path.join(
-    path_str,
-    'start_test',
-    'esindex',
-    esindex
-  ))
-  console.log(`Importing esindex: ${esindex}`)
-  importTxs()
+  for (let esindex of esindexes){
+    const { importTxs } = await importIndex (esClient, esindex, path.join(
+      path_str,
+      'start_test',
+      'esindex',
+      esindex
+    ))
+    console.log(`Importing esindex: ${esindex}`)
+    importTxs()
+  }
 }
 
 run()
